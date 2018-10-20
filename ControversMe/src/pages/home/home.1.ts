9dcc68925cf295c3f243f254d1669a27f0ca0479
RonArel <RonusTheBoss123@gmail.com>
2018-10-20 12:41:28 -0700

    Serve Update

--- ControversMe/src/pages/home/home.1.ts
+++ ControversMe/src/pages/home/home.1.ts
@@ -62,4 +62,26 @@ export class HomePage1 {
 
         toast.present();
     }
+    sendPostRequest() {
+        var headers = new Headers();
+        headers.append("Accept", 'application/json');
+        headers.append('Content-Type', 'application/json' );
+        const requestOptions = new RequestOptions({ headers: headers });
+    
+        let postData = {
+                "last_name": "Customer004",
+                "first_name": "Customer004",
+                "email": "customer004@email.com",
+                "bio": "0000252525",
+                "password": "0000252525"
+                
+        }
+    
+        this.http.post("http://127.0.0.1:3000/customers", postData, requestOptions)
+          .subscribe(data => {
+            console.log(data['_body']);
+           }, error => {
+            console.log(error);
+          });
+        }
 }
