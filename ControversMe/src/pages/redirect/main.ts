7ec01afa63e14d2b6e5cd46b1dd80ff701c52699
RonArel <RonusTheBoss123@gmail.com>
2018-10-20 12:21:40 -0700

    Chat Rooms

---
+++ ControversMe/src/pages/redirect/main.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { Headers, Http, RequestOptions } from '@angular/http';
+import { NavController, ToastController } from 'ionic-angular';
+import { HomePage1 } from '../home/home.1';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'main.html'
+})
+export class Main {
+  cred = { username: '', password: ''};  
+  constructor(public navCtrl: NavController, public http: Http, private toastCtrl: ToastController) {
+ 
+  }
+  signIn(){
+    if (this.cred.username != ""){
+      if (this.cred.password != ""){
+      
+      }else{
+        this.presentToast("No Password");        
+      }
+    }else{
+      this.presentToast("No Username");      
+    }
+  }
+  register() {
+    this.navCtrl.push(HomePage1);
+  }
+  presentToast(text: string) {
+    let toast = this.toastCtrl.create({
+      message: text,
+      duration: 3000,
+      position: 'top'
+    });
+  
+    toast.onDidDismiss(() => {
+      console.log('Dismissed toast');
+    });
+  
+    toast.present();
+  }
+}
