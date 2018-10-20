df78cfe480e4cc9c1283a4ced8223cd00968eac0
RonArel <RonusTheBoss123@gmail.com>
2018-10-20 16:31:04 -0700

    this

--- ControversMe/src/pages/mainpage/main.ts
+++ ControversMe/src/pages/mainpage/main.ts
@@ -31,7 +31,7 @@ export class Main {
     });
   }
   buttonClick(){
-    this.storage.set('chatroom', ids);
+    this.storage.set('chatroom', this.ids);
     this.navCtrl.push(MainRedirect);    
   }
 }
